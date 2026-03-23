49c3c367db35e674c10df0e763e26453a4085c91
Julius-Lamutt <jel57@njit.edu>
2026-03-22 23:27:03 -0400

    added shuriken item files to the game

---
+++ include/item_shuriken.h
@@ -0,0 +1,12 @@
+#ifndef __ITEM_SHURIKEN_H__
+#define __ITEM_SHURIKEN_H__
+
+#include "entity.h"
+
+/**
+* @brief spawn a shuriken
+* return NULL on error, a pointer to a shuriken otherwise
+*/
+Entity *shuriken_new();
+
+#endif
