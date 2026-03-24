f52e5182fa1a757c29265adba7185d9decf40c63
Julius-Lamutt <jel57@njit.edu>
2026-03-24 08:32:13 -0400

    added files for the objectives menu

---
+++ include/objectives_menu.h
@@ -0,0 +1,13 @@
+#ifndef __OBJECTIVES_MENU_H__
+#define __OBJECTIVES_MENU_H__
+
+#include <SDL.h>
+#include "windows.h"
+
+/*
+* @brief load the objectives menu
+* @return NULL on error, a pointer to the main menu window otherwise
+*/
+Window *objectives_menu();
+
+#endif
